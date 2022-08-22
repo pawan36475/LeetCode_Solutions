600fc6e08462b8c3839691970ded38bbb9b5d82b
Pawan Deep Singh <82301521+pawan36475@users.noreply.github.com>
2022-08-22 20:07:43 +0530

    Update TwoSum.java

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,5 +1,6 @@
 /* Link to problem: https://leetcode.com/problems/two-sum/ */
 
+/*Time complexity=O(n^2)*/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         ArrayList<Integer> list = new ArrayList<Integer>();
@@ -21,3 +22,41 @@ class Solution {
         return actualArray;
 }
 }
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*Time Complexity=O(n)      [Optimized Solution]*/
+    
+
+public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        int complement = 0;
+        for (int i = 0; i < nums.length; i++) {
+            complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        return null;
+    }
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*Time Complexity=O(n)    [Optimized Solution]*/
+
+ public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int complement = 0;
+        for (int i = 0; i < nums.length; i++) {
+            complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }

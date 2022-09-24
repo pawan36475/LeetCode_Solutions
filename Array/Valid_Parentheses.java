4d8971c6ffd03ef525ef1ca6ad46d6c1b51cf9b5
Pawan Deep Singh <82301521+pawan36475@users.noreply.github.com>
2022-09-24 20:07:36 +0530

    Update Valid_Parentheses.java

--- Array/Valid_Parentheses.java
+++ Array/Valid_Parentheses.java
@@ -4,6 +4,9 @@
 class Solution {
     public boolean isValid(String s) {
         int len = s.length();
+//         if(len%2==1){    //can be used to reduce the time
+//             return false;
+//         }
         char[] ch = s.toCharArray();
         System.out.println(ch.length);
         Stack<Character> stk = new Stack<>();
